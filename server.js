c4a389959fdfdbf214a03132ab41426d0abf8960
Loïs L <36632834+LifestreamVII@users.noreply.github.com>
2023-03-18 03:30:00 +0100

    remove

--- server.js
+++
@@ -1,9 +0,0 @@
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("db/users.json");
-const middlewares = jsonServer.defaults({ static: "./build" });
-const port = process.env.PORT || 8000;
-server.use(middlewares);
-
-server.use(router);
-server.listen(port);
