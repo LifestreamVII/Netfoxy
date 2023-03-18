d2acc4a71c25b3773a7622ffa6bad704d56f73d8
Loïs L <36632834+LifestreamVII@users.noreply.github.com>
2023-03-18 02:42:56 +0100

    test serv

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-web: serve -s build
-api: json-server --watch db/users.json --port 8000
++web: node server.js
---
+++ server.js
@@ -0,0 +1,9 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db/users.json");
+const middlewares = jsonServer.defaults({ static: "./build" });
+const port = process.env.PORT || 8000;
+server.use(middlewares);
+
+server.use(router);
+server.listen(port);
